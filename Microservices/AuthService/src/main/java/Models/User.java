2b96f32dc2d3096bcc3a6ce3372fe2f8e090527f
Omar Nour <omar.syd.n@gmail.com>
2025-04-30 01:00:39 +0300

    AuthService and User Model

---
+++ Microservices/AuthService/src/main/java/Models/User.java
@@ -0,0 +1,86 @@
+package Models;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(columnDefinition = "UUID", updatable = false, nullable = false)
+    private UUID id;
+    private String username;
+    private String password;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String role;
+
+    public User() {
+    }
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRoleCustomer() {
+        this.role = "customer";
+    }
+
+    public void setRoleBuyer() {
+        this.role = "seller";
+    }
+}
