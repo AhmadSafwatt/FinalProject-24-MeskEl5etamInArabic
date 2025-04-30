21e59982cf94507b825cb6e237007917196046e5
MoTammaa <mohamed.abotammaa@student.guc.edu.eg>
2025-04-30 16:33:30 +0300

    added Order Microservice base

---
+++ Microservices/OrderService/src/test/java/com/homechef/OrderService/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homechef.OrderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
