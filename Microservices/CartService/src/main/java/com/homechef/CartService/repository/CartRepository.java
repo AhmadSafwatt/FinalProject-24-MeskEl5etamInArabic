a6253d29e77022f57ef2b8b95ad39da4a91f4fc4
Ahmad Safwat <ahmad.safwat.elewa@gmail.com>
2025-04-30 18:47:06 +0300

    Cart Base Files
    - Cart Service, Repo, Controller

---
+++ Microservices/CartService/src/main/java/com/homechef/CartService/repository/CartRepository.java
@@ -0,0 +1,11 @@
+package com.homechef.CartService.repository;
+
+import com.homechef.CartService.model.Cart;
+import java.util.UUID;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends MongoRepository<Cart, UUID> {
+
+}
