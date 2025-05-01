ba50fe8aade38ce2c056a03b46c9082ced4fd0c9
Omar Hesham <omar.hesham-youssef@outlook.com>
2025-05-01 19:37:30 +0300

    Update CassandraConfig

--- Microservices/ChatService/src/main/java/com/example/chatservice/configurations/CassandraConfig.java
+++ Microservices/ChatService/src/main/java/com/example/chatservice/configurations/CassandraConfig.java
@@ -4,13 +4,14 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
 import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
 
 import java.util.List;
 
 @Configuration
-public class CassandraInitConfig extends AbstractCassandraConfiguration {
+public class CassandraConfig extends AbstractCassandraConfiguration {
 
     @Value("${spring.cassandra.keyspace-name}")
     private String keyspaceName;
@@ -22,6 +23,11 @@ public class CassandraInitConfig extends AbstractCassandraConfiguration {
     }
 
     @Override
+    public @NotNull SchemaAction getSchemaAction() {
+        return SchemaAction.CREATE_IF_NOT_EXISTS;
+    }
+
+    @Override
     protected @NotNull List<CreateKeyspaceSpecification> getKeyspaceCreations() {
         CreateKeyspaceSpecification specification = CreateKeyspaceSpecification
                 .createKeyspace(getKeyspaceName())
@@ -30,5 +36,10 @@ public class CassandraInitConfig extends AbstractCassandraConfiguration {
                 .withSimpleReplication(1);
 
         return List.of(specification);
+    }
+
+    @Override
+    public @NotNull String[] getEntityBasePackages() {
+        return new String[] { "com.example.chatservice.models" };
     }
 }
