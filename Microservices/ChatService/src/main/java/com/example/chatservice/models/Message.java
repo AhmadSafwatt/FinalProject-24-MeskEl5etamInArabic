c2de676ecda08e0eb109b56ea5e48f81efd35955
Omar Hesham <omar.hesham-youssef@outlook.com>
2025-05-01 20:42:44 +0300

    Change timestamp type to java.util.Date

--- Microservices/ChatService/src/main/java/com/example/chatservice/models/Message.java
+++ Microservices/ChatService/src/main/java/com/example/chatservice/models/Message.java
@@ -7,6 +7,7 @@ import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 
 import java.time.Instant;
+import java.util.Date;
 import java.util.UUID;
 
 import com.example.chatservice.enums.MessageStatus;
@@ -18,8 +19,38 @@ import com.example.chatservice.enums.MessageType;
 
 @Setter
 @Getter
-@Table(value = "messages",keyspace = "chat_keyspace")
+@Table(value = "messages")
 public class Message {
+
+    public Message() {
+        this.id = UUID.randomUUID();
+        this.timestamp = Date.from(Instant.now());
+    }
+
+    public Message(UUID sender, UUID receiver, String content, MessageStatus status, MessageType type) {
+        this.id = UUID.randomUUID();
+        this.sender = sender;
+        this.receiver = receiver;
+        this.content = content;
+        this.timestamp = Date.from(Instant.now());
+        this.status = status;
+        this.type = type;
+    }
+
+    public Message(UUID sender, UUID receiver, String content, MessageType type) {
+        this.id = UUID.randomUUID();
+        this.sender = sender;
+        this.receiver = receiver;
+        this.content = content;
+        this.timestamp = Date.from(Instant.now());
+        this.type = type;
+    }
+
+    public Message(String content) {
+        this.id = UUID.randomUUID();
+        this.content = content;
+        this.timestamp = Date.from(Instant.now());
+    }
     @PrimaryKey
     private UUID id;
 
@@ -29,7 +60,7 @@ public class Message {
 
     private String content;
 
-    private Instant timestamp;
+    private Date timestamp;
 
     private MessageStatus status;
     private MessageType type;
