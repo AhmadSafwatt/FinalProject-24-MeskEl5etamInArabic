5a12d99981ee03597af89bbed47774df11accb45
Omar Nour <omar.syd.n@gmail.com>
2025-05-03 01:55:01 +0300

    Partial Progress For AuthService

---
+++ Microservices/AuthService/src/main/java/Repositories/UserRepository.java
@@ -0,0 +1,23 @@
+package Repositories;
+
+import Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+
+    User findByPhoneNumber(String phoneNumber);
+
+    User findByAddress(String address);
+
+    User findByRole(String role);
+
+
+}
+
---
+++ Microservices/AuthService/src/main/java/Services/AuthService.java
@@ -0,0 +1,70 @@
+package Services;
+
+import Models.User;
+import Repositories.UserRepository;
+import jakarta.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+
+
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AuthService(UserRepository userRepository , PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public String registerUser(User user) {
+        if (userRepository.findByUsername(user.getUsername()) != null) {
+            return "Username already exists";
+        }
+        if (userRepository.findByEmail(user.getEmail()) != null) {
+            return "Email already exists";
+        }
+        // check if valid email format
+        if (!user.getEmail().matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
+            return "Invalid email format";
+        }
+
+        // hash password
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoleUnverifiedUser();
+        User newUser = userRepository.save(user);
+
+        // verify email by sending url with user.getId() and user.getEmail() to the email
+        sendEmailVerificationLink(user.getEmail(), newUser.getId());
+        return "Verification Email Sent";
+    }
+
+    public String sendEmailVerificationLink(String email, UUID id) {
+        //TODO: send email verification link, the link should be the url of verifyEmail with value of id
+        return "TODO";
+    }
+
+    public String verifyEmail(UUID id) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return "User not found";
+        }
+
+        if (user.getRole().equals("unverified_user")) {
+            user.setRoleCustomer();
+        }
+        else {
+            user.setRoleSeller();
+        }
+
+        userRepository.save(user);
+        return "Email Verified";
+    }
+}
