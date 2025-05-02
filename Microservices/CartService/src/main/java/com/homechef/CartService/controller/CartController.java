e7af9c15473a1baa990d35d169b86613ce2beab4
M-Bahy <mohamedayman2942002@gmail.com>
2025-05-02 14:00:00 +0300

    Add endpoint to retrieve cart by customer ID in CartController

--- Microservices/CartService/src/main/java/com/homechef/CartService/controller/CartController.java
+++ Microservices/CartService/src/main/java/com/homechef/CartService/controller/CartController.java
@@ -1,9 +1,16 @@
 package com.homechef.CartService.controller;
 
+import com.homechef.CartService.model.Cart;
 import com.homechef.CartService.service.CartService;
+
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/carts")
@@ -11,4 +18,10 @@ public class CartController {
 
     @Autowired
     private CartService cartService;
+
+
+    @GetMapping("/customerId/{customerId}")
+    public Cart getCartByCustomerId(@PathVariable UUID customerId) {
+        return cartService.getCartByUserId(customerId);
+    }
 }
