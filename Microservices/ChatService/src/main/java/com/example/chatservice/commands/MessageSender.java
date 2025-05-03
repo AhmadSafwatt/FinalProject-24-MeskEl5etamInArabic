1344a9d4d6b6ed15259dd75e3442a7a600fdd3f8
Omar Hesham <omar.hesham-youssef@outlook.com>
2025-05-03 03:15:56 +0300

    Delete MessageSender.java

--- Microservices/ChatService/src/main/java/com/example/chatservice/commands/MessageSender.java
+++
@@ -1,9 +0,0 @@
-package com.example.chatservice.commands;
-
-import com.example.chatservice.commands.Command;
-
-public class MessageSender {
-    public void send(Command command) {
-        command.execute();
-    }
-}
