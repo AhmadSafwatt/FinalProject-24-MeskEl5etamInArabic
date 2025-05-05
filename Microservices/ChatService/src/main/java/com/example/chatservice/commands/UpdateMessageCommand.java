c4d1eb53d331aca713afabb477a52637aa995324
Omar Hesham <omar.hesham-youssef@outlook.com>
2025-05-05 03:40:31 +0300

    Modify UpdateMessageCommand

--- Microservices/ChatService/src/main/java/com/example/chatservice/commands/UpdateMessageCommand.java
+++ Microservices/ChatService/src/main/java/com/example/chatservice/commands/UpdateMessageCommand.java
@@ -3,20 +3,22 @@ package com.example.chatservice.commands;
 import com.example.chatservice.models.Message;
 import com.example.chatservice.services.MessageService;
 
+import java.util.UUID;
+
 public class UpdateMessageCommand implements Command {
 
-    private final Message message;
+    private final UUID messageId;
+    private final Message partialMessage;
     private final MessageService messageService;
 
-    public UpdateMessageCommand(Message message, MessageService messageService) {
-        this.message = message;
+    public UpdateMessageCommand(UUID messageId, Message partialMessage, MessageService messageService) {
+        this.messageId = messageId;
+        this.partialMessage = partialMessage;
         this.messageService = messageService;
     }
 
     @Override
     public void execute() {
-        if (message != null) {
-            messageService.saveMessage(message);
-        }
+        messageService.updateMessage(messageId, partialMessage);
     }
 }
