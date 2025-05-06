08c02098bf2690de144415082b973b2dfc21dabf
Hosain Ghoraba <hussain.ghoraba@gmail.com>
2025-05-06 17:18:52 +0300

    Read Order for buyer done

--- Microservices/OrderService/src/main/java/com/homechef/OrderService/controllers/OrderController.java
+++ Microservices/OrderService/src/main/java/com/homechef/OrderService/controllers/OrderController.java
@@ -1,13 +1,16 @@
 package com.homechef.OrderService.controllers;
 
 import com.homechef.OrderService.models.Order;
+import com.homechef.OrderService.models.OrderItem;
 import com.homechef.OrderService.services.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/orders")
@@ -15,8 +18,17 @@ public class OrderController {
     private OrderService orderService;
 
     @Autowired
-    public OrderController(OrderService orderService) {this.orderService = orderService;}
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
     @GetMapping("/all")
-    public List<Order> getAllOrders() {return orderService.getAllOrders();}
+    public List<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @GetMapping("/{orderId}/items")
+    public List<OrderItem> getOrderItems(@PathVariable UUID orderId) {
+        return orderService.getOrderItems(orderId);
+    }
 }
--- Microservices/OrderService/src/main/java/com/homechef/OrderService/services/OrderService.java
+++ Microservices/OrderService/src/main/java/com/homechef/OrderService/services/OrderService.java
@@ -1,18 +1,30 @@
 package com.homechef.OrderService.services;
 
 import com.homechef.OrderService.models.Order;
+import com.homechef.OrderService.models.OrderItem;
 import com.homechef.OrderService.repositories.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class OrderService {
     private OrderRepository orderRepository;
 
     @Autowired
-    public OrderService(OrderRepository orderRepository) {this.orderRepository = orderRepository;}
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
 
-    public List<Order> getAllOrders() { return orderRepository.findAll(); }
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    public List<OrderItem> getOrderItems(UUID orderId) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new RuntimeException("Order with ID " + orderId + " not found"));
+        return order.getItems();
+    }
 }
