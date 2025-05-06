ff0a0b153305b3d2ed326e8ed4bdcf4f6b667163
Hosain Ghoraba <hussain.ghoraba@gmail.com>
2025-05-06 17:05:31 +0300

    Update OrderRepository to use UUID as the identifier type instead of <Long>

--- Microservices/OrderService/src/main/java/com/homechef/OrderService/repositories/OrderRepository.java
+++ Microservices/OrderService/src/main/java/com/homechef/OrderService/repositories/OrderRepository.java
@@ -1,10 +1,13 @@
 package com.homechef.OrderService.repositories;
 
 import com.homechef.OrderService.models.Order;
+
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, UUID> {
 
 }
