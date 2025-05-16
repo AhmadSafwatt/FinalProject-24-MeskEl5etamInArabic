1f5558229c08c95e8b639a31c10bf76a85e6ec9e
Omar Hesham <omar.hesham-youssef@outlook.com>
2025-05-16 13:54:41 +0300

    Change TokenPrinter to use TokenUtil

--- Microservices/ChatService/src/main/java/com/example/chatservice/utils/TokenPrinter.java
+++ Microservices/ChatService/src/main/java/com/example/chatservice/utils/TokenPrinter.java
@@ -4,19 +4,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
+import java.util.UUID;
 
 @Slf4j
 @Component
 public class TokenPrinter implements CommandLineRunner {
     @Override
     public void run(String... args) {
-        String username = "testuser";
-        String token = JwtUtil.generateToken(username, new HashMap<>());
-        System.out.println("Generated JWT at startup for '" + username + "':\n" + token);
 
-        String usernameFromToken = JwtUtil.extractUsername(token);
-        System.out.println("Username extracted from token: " + usernameFromToken);
+        UUID userId = UUID.randomUUID();
+
+        String token = TokenUtil.generateTestToken(userId);
+        System.out.println("Generated JWT at startup for '" + userId + "':\n" + token);
+
+        String userIdFromToken = JwtUtil.extractUserId(token);
+        System.out.println("userId extracted from token: " + userIdFromToken);
         boolean isValid = JwtUtil.validateToken(token);
         if (isValid) {
             System.out.println("Token is valid.");
