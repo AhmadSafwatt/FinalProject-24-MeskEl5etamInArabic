4dd4d88d64f280999139a2526d7060ce71a72b8e
Omar Hesham <omar.hesham-youssef@outlook.com>
2025-05-16 14:12:52 +0300

    Minor update in TokenPrinter print format

--- Microservices/ChatService/src/main/java/com/example/chatservice/utils/TokenPrinter.java
+++ Microservices/ChatService/src/main/java/com/example/chatservice/utils/TokenPrinter.java
@@ -15,10 +15,10 @@ public class TokenPrinter implements CommandLineRunner {
         UUID userId = UUID.randomUUID();
 
         String token = TokenUtil.generateTestToken(userId);
-        System.out.println("Generated JWT at startup for '" + userId + "':\n" + token);
+        System.out.println("Generated JWT at startup for " + userId + ":\n" + token);
 
         String userIdFromToken = JwtUtil.extractUserId(token);
-        System.out.println("userId extracted from token: " + userIdFromToken);
+        System.out.println("id extracted from token: " + userIdFromToken);
         boolean isValid = JwtUtil.validateToken(token);
         if (isValid) {
             System.out.println("Token is valid.");
